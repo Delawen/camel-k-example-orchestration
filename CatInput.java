7c9c5005df5647ed6a17350c43106fcd8494cc57
Maria Arias de Reyna <ariasdereyna@redhat.com>
2020-09-23 12:48:53 +0200

    Refactor: kamel-config.yaml and jitpack

--- CatInput.java
+++ CatInput.java
@@ -1,10 +1,8 @@
 // camel-k: language=java
-// camel-k: language=java property-file=orchestration.properties  dependency=camel:gson
-// camel-k: source=customizers/PostgreSQLCustomizer.java 
-
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import model.Cat;
 
 public class CatInput extends RouteBuilder {
   @Override
@@ -21,34 +19,5 @@ public class CatInput extends RouteBuilder {
         
     //Write some log to know it finishes properly
     .log("Cat stored.");
-      
   }
-
-class Cat {
-
-  private String name;
-  private String image;
-
-  public String name() {
-      return name;
-  }
-
-  public void setName(String name) {
-      this.name = name;
-  }
-
-  public String image() {
-      return image;
-  }
-
-  public void setImage(String image) {
-      this.image = image;
-  }
-
-  @Override
-  public String toString() {
-    return "Cat [image=" + image + ", name=" + name + "]";
-  }
-}
-
 }
